ad9e18179aa8c8414c782a67dda82aa3ee00c9bc
shukurilloummatqulov-bit <shukurilloummatqulov@gmail.com>
2026-06-06 15:33:12 +0500

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,28 @@
+import telebot
+
+TOKEN = "8977018536:AAESKjDng5xEr_vSGfyiO8udsyEOsyVbiMk"
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(func=lambda message: True)
+def reply_to_all(message):
+    if message.from_user.is_bot:
+        return
+
+    user = message.from_user
+    username = user.username
+    
+    if username:
+        mention = f"@{username}"
+    else:
+        mention = user.first_name
+
+    text = f"{mention} rahmat ey oq kongil inson JAMA bekor chiqib ketdida"
+
+    try:
+        bot.reply_to(message, text)
+    except:
+        pass
+
+print("Bot ishga tushdi...")
+bot.infinity_polling()
