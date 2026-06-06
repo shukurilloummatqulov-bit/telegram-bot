b00d059b793a999ba87f12b2b1508b43d1b212c7
shukurilloummatqulov-bit <shukurilloummatqulov@gmail.com>
2026-06-06 17:09:29 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,14 +2,13 @@ import os
 import requests
 import telebot
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-GROK_API_KEY = os.getenv("GROK_API_KEY")
+BOT_TOKEN = os.getenv("8977018536:AAESKjDng5xEr_vSGfyiO8udsyEOsyVbiMk")
+GROK_API_KEY = os.getenv("xai-xw8y83z0k8zqB2MN8ZtOldIKjUu2eh9VtQCK5RYjnvR6nhQMrbt6ZvvqnrHzSrUDmo1qjmVax8jY5avA")
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(func=lambda message: True)
 def chat(message):
-
     response = requests.post(
         "https://api.x.ai/v1/chat/completions",
         headers={
@@ -17,15 +16,20 @@ def chat(message):
             "Content-Type": "application/json"
         },
         json={
-            "model": "grok-4",
+            "model": "grok-beta",
             "messages": [
                 {"role": "user", "content": message.text}
             ]
         }
     )
 
-    answer = response.json()["choices"][0]["message"]["content"]
+    data = response.json()
+    print(data)  # Railway logda ko‘rish uchun
 
-    bot.reply_to(message, answer)
+    if "choices" in data:
+        answer = data["choices"][0]["message"]["content"]
+        bot.reply_to(message, answer)
+    else:
+        bot.reply_to(message, f"Xato: {data}")
 
-bot.infinity_polling()
+bot.polling()
